d76a1179bf8d53c0e0d1c9ac58f496b6df5e9de8
Fortahr <54455828+Fortahr@users.noreply.github.com>
2023-04-09 18:42:01 +0200

    Config file clean up and documentation

--- inc/ecs/config.h
+++ inc/ecs/config.h
@@ -4,19 +4,21 @@
 #include ECS_REGISTRY_INCLUDE
 #endif
 
-namespace ecs::config
-{
-#ifndef ECS_REGISTRY_CLASS
-	static_assert(false, "Missing ECS registry, define the ECS_REGISTRY_CLASS macro or edit this config.");
-#else
-	typedef ECS_REGISTRY_CLASS Registry;
+#ifndef ECS_BUCKET_SIZE
+#define ECS_BUCKET_SIZE 64
 #endif
 
-#ifndef ECS_BUCKET_SIZE
-	constexpr size_t bucket_size = 64;
-#else
-	constexpr size_t bucket_size = ECS_BUCKET_SIZE;
+#ifndef ECS_REGISTRY_CLASS
+static_assert(false, "Missing ECS registry, define the ECS_REGISTRY_CLASS macro or edit this config file.");
 #endif
+
+namespace ecs::config
+{
+	// The global registry, should be (a sub-class of) ecs::registry<...>
+	typedef ECS_REGISTRY_CLASS Registry;
+
+	// Amount of entities (their components) that will be stored in each bucket
+	constexpr size_t bucket_size = ECS_BUCKET_SIZE;
 
 	// entities use 32 bits to define the world they belong to and the reuse version, inclusive.
 	// define how much of these bits are reserved for the world; 
